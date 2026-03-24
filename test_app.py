3f12b4cd0de2f8454b7533daf906611122c9a5ea
samermk12 <samerangel13@gmail.com>
2026-03-25 00:11:10 +0100

    Trigger email notification

--- test_app.py
+++ test_app.py
@@ -4,5 +4,5 @@ from app import app
 def test_index():
     tester = app.test_client()
     response = tester.get('/')
-    assert response.status_code == 500
+    assert response.status_code == 200
     assert b"Hello, World!" in response.data
